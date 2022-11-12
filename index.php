a88bdee293319ae2e9a3591a92a89b3aedb7cfb0
lionel <lionelmonthe184@gmail.com>
2022-11-12 16:34:30 +0100

    modif4

--- index.php
+++ index.php
@@ -82,7 +82,10 @@
         -webkit-overflow-scrolling: touch;
       }
 
-      .navbar {
+      .text-bg-aqua {
+        background-color: teal;
+      }
+      .mb-4{
         background-color: aqua;
       }
     </style>
@@ -127,11 +130,11 @@
 </svg>
     
   <header>
-    <div class="px-3 py-2 text-bg-dark">
+    <div class="px-3 py-2 text-bg-aqua">
       <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-<img src="../static/Logo.png" width="150px" alt="logo">
+          <img src="static/Logo.png" width="300px" alt="logo">
           </a>
 
           <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
@@ -169,7 +172,7 @@
         </div>
       </div>
     </div>
-    <div class="px-3 py-2 border-bottom mb-3">
+    <div class="px-3 py-2 border-bottom mb-4">
       <div class="container d-flex flex-wrap justify-content-center">
         <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
           <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
